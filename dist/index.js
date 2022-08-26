9f329429513f7846cf2042077e261aaf9856548b
Denis <dsapaiv@gmail.com>
2022-08-26 16:19:35 +0300

    Rollup setup

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+
+
+function ___$insertStyle(css) {
+    if (!css || typeof window === 'undefined') {
+        return;
+    }
+    const style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.innerHTML = css;
+    document.head.appendChild(style);
+    return css;
+}
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function PayButton() {
+    return 4;
+}
+
+exports["default"] = PayButton;
+//# sourceMappingURL=index.js.map
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+export default function PayButton() {
+  return 4;
+}
