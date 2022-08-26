fa2e6943c1988500b153e4044a6e940d574b91d9
Denis <dsapaiv@gmail.com>
2022-08-26 17:35:16 +0300

    more work done on button

--- src/CoinablePayButton.tsx
+++ src/CoinablePayButton.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, MouseEvent } from 'react';
 import Solana from './Solana';
 
 import { CoinablePayButtonProps } from './types';
@@ -6,9 +6,56 @@ import { CoinablePayButtonProps } from './types';
 import './styles.scss';
 
 const CoianblePayButton = forwardRef<HTMLButtonElement, CoinablePayButtonProps>(
-  ({ children = 'Pay', ...props }, ref) => {
+  (
+    {
+      children = 'Pay',
+      onFailure,
+      onSuccess,
+      productId,
+      quantity = 1,
+      requestCurrency = 'USD',
+      ...props
+    },
+    ref
+  ) => {
+    const handleOnClick = async (
+      e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+    ) => {
+      e.preventDefault();
+
+      const data = {
+        product_id: productId,
+        quantity,
+        request_currency: requestCurrency,
+      };
+
+      try {
+        const response = await fetch(
+          'https://api.coinablepay.com/v1/api/checkouts/single',
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+          }
+        );
+        const resp: Record<string, any> = await response.json();
+        if (resp.redirect_url) onSuccess(resp.redirect_url);
+        else
+          onFailure(
+            "Unexpected error, fetch succedded but couldn't get redirect url from Coinable, try again in a few seconds."
+          );
+      } catch (error) {
+        onFailure(error.message);
+      }
+    };
+
     return (
-      <button ref={ref} className="coinable-pay-button" {...props}>
+      <button
+        ref={ref}
+        className="coinable-pay-button"
+        onClick={handleOnClick}
+        {...props}
+      >
         <div className="coinable-pay-button-content">
           <Solana />
           {children}
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,9 @@ import { ReactNode } from 'react';
 
 export interface CoinablePayButtonProps {
   children?: ReactNode;
+  productId: string;
+  onSuccess: (redirectLink: string) => void;
+  onFailure: (errorMessage: string) => void;
+  quantity?: number;
+  requestCurrency?: string;
 }
